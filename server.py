37f66856cfe106387ae5868b36dda71bcbe81da0
atarax-dev <83333973+atarax-dev@users.noreply.github.com>
2022-01-14 15:01:14 +0100

    Update server.py
    Created error messages on booking issues

--- server.py
+++ server.py
@@ -51,6 +51,15 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+    if placesRequired > int(competition['numberOfPlaces']):
+        flash("Not enough places to book")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    elif placesRequired < 0:
+        flash("You can't use negative numbers")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    elif placesRequired > 12:
+        flash("You can't book more than 12 places")
+        return render_template('welcome.html', club=club, competitions=competitions)
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
